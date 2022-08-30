52fe5fa4e9e72dc390dafdc1f3f8ba119030c6f4
EduSaavedra <marcos.saavedra@live.com>
2022-08-29 21:12:41 -0300

    Alteracao no script de teste do APP

--- teste-app.sh
+++ teste-app.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 RESULT="'wget -qO- http://localhost:8090'"
 wget -q localhost:8090
-if [$? -eq 0] then
+if [ $? -eq 0 ] 
+then
     echo 'Sucesso - Serviço no ar!'
-elif [{ $RESULT == *"Number"* }] then
+elif [{ $RESULT == *"Number"* }] 
+then
     echo 'Sucesso - Numero de visitas'
     echo $RESULT
 else
